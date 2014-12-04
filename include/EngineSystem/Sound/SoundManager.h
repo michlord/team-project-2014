82203ceedbf22c701d873fb86f72f2fd83b92b40
Piotr Pytlik <piotrcpytlik@gmail.com>
2014-12-04 21:10:07 +0100

    [#16] Fixed initial bugs caused by incorrect usage of Music.

--- include/EngineSystem/Sound/SoundManager.h
+++ include/EngineSystem/Sound/SoundManager.h
@@ -7,41 +7,40 @@
 #include <unordered_map>
 
 class SoundManager {
-	public:
-		SoundManager();
-		~SoundManager();
+    public:
+        SoundManager();
+        ~SoundManager();
 
-		void clear();
+        void clear();
 
-		void setSoundsVolume(float);
-		void setMusicsVolume(float);
+        void setIdGenBase(int);
+        void setSoundsVolume(float);
+        void setMusicsVolume(float);
 
-		void scaleSoundsVolume(float);
-		void scaleMusicsVolume(float);
+        void scaleSoundsVolume(float);
+        void scaleMusicsVolume(float);
 
-		std::size_t createBuffer(const std::string &);
-		std::size_t createSound(std::size_t);
-		std::size_t createMusic(const std::string &);
+        std::size_t createBuffer(const std::string &);
+        std::size_t createSound(std::size_t);
+        std::size_t createMusic(const std::string &);
 
-		void deleteSound(std::size_t);
-		void deleteMusic(std::size_t);
+        void deleteSound(std::size_t);
+        void deleteMusic(std::size_t);
 
-		// TODO: find out if these should be static?
-		sf::Sound& getSound(std::size_t);
-		sf::Music& getMusic(std::size_t);
+        void stopSounds();
+        void stopMusics();
 
-		void stopSounds();
-		void stopMusics();
+    private:
+        int id_gen_base;
+        float sounds_volume;
+        float musics_volume;
 
-	private:
-		int id_gen_base;
-		float sounds_volume;
-		float musics_volume;
+        std::unordered_map<std::size_t, sf::SoundBuffer> sound_buffers;
+        std::unordered_map<std::size_t, sf::Sound> sounds;
+        std::unordered_map<std::size_t, sf::Music*> musics;
 
-// TODO: find out if this will cause problems, because sf::Sound uses const SoundBuffer...
-		std::unordered_map<std::size_t, sf::SoundBuffer> sound_buffers;
-		std::unordered_map<std::size_t, sf::Sound> sounds;
-		std::unordered_map<std::size_t, sf::Music> musics;
+        sf::Sound& getSound(std::size_t);
+        sf::Music& getMusic(std::size_t);
 };
 
 #endif // !SOUNDMANAGER_H_INCLUDED
