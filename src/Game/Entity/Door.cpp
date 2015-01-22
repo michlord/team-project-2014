528ef4ef48c99cf3bed5efa23224265448938214
zyks <pazy011@gmail.com>
2015-01-22 19:02:31 +0100

    [#30] added door entity

---
+++ src/Game/Entity/Door.cpp
@@ -0,0 +1,35 @@
+#include <Game/Entity/Door.h>
+
+namespace Entity {
+
+    Door::Door(int id) : Entity::BaseEntity(id) {
+        Core::frameContext.assetsManager->loadTexture("assets/images/door.png", "door");
+
+        sprite = new Video::Render::Sprite();
+        sprite->bindTexture(Core::frameContext.assetsManager->getTexture("door"));
+
+        Video::Render::Sprite::Frame frame;
+        frame.setTextureSegment(sf::IntRect(0, 0, 60, 106));
+    }
+
+    Door::~Door() {
+
+    }
+
+    bool Door::handleMessage(const Message& msg) {
+        if (msg.msg == Msg::NextLvl) {
+            Core::frameContext.sceneStack->popScene();
+            Core::frameContext.sceneStack->pushScene(new Scene::Gameplay(frameContext.sceneStack.get(), 1));
+                                                     //levelManager.getCurrentID() + 1));
+            return true;
+        }
+
+        return false;
+    }
+
+    void Door::draw(sf::RenderTarget& target, sf::RenderStates states = sf::RenderStates::Default)
+    {
+        if (sprite)
+            sprite->draw(target, states);
+    }
+}
