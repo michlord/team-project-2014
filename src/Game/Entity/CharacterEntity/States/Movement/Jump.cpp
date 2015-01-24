60d6a1c9ba11bf16b050b92a3f7630eeb8c457d3
RippeR <ripper37@gmail.com>
2015-01-24 15:58:00 +0100

    [#41] Sound for jumping added.

--- src/Game/Entity/CharacterEntity/States/Movement/Jump.cpp
+++ src/Game/Entity/CharacterEntity/States/Movement/Jump.cpp
@@ -1,8 +1,25 @@
 #include <Game/Entity/CharacterEntity/States.h>
 
+#include <sfml/Audio.hpp>
+
 namespace Entity {
 
     void Jump::onEnter(CharacterEntity *entity){
+        static sf::SoundBuffer buffer;
+        static sf::Sound sound;
+        static bool initializedSound = false;
+
+        if(initializedSound == false) {
+            initializedSound = true;
+
+            if(!buffer.loadFromFile("assets/sound/player_jump.wav")) {
+                Log::get().write(Log::System::Game, "Could not load sound player_jump.wav");
+            } else {
+                sound.setBuffer(buffer);
+            }
+        }
+
+        sound.play();
         entity->animation.setCurrentSequence("jump");
     }
 
